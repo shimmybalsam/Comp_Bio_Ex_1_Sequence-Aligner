bcc4777dfbb4b8836a685e7eb9e45c308c73305c
shimmybalsam <38699071+shimmybalsam@users.noreply.github.com>
2021-01-14 21:06:33 +0200

    Add files via upload

---
+++ seq_align.py
@@ -0,0 +1,31 @@
+import argparse
+import numpy as np
+from itertools import groupby
+
+def fastaread(fasta_name):
+    f = open(fasta_name)
+    faiter = (x[1] for x in groupby(f, lambda line: line.startswith(">")))
+    for header in faiter:
+        header = next(header)[1:].strip()
+        seq = "".join(s.strip() for s in next(faiter))
+        yield header, seq
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('seq_a', help='Path to first FASTA file (e.g. fastas/HomoSapiens-SHH.fasta)')
+    parser.add_argument('seq_b', help='Path to second FASTA file')
+    parser.add_argument('--align_type', help='Alignment type (e.g. local)', required=True)
+    parser.add_argument('--score', help='Score matrix in.tsv format (default is score_matrix.tsv) ', default='score_matrix.tsv')
+    command_args = parser.parse_args()
+    if command_args.align_type == 'global':
+        raise NotImplementedError
+    elif command_args.align_type == 'local':
+        raise NotImplementedError
+    elif command_args.align_type == 'overlap':
+        raise NotImplementedError
+    # print the best alignment and score
+
+
+if __name__ == '__main__':
+    main()
